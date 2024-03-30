d896e3afc1de18b57474017de385e496266a4971
Supertolek <supertolekmc@gmail.com>
2024-03-30 17:21:07 +0100

    Summary

--- scripts/load.js
+++ scripts/load.js
@@ -1,11 +1,11 @@
-import data from "../data.json" assert {type: "json"};
+import data from "../data.json" with {type: "json"};
 console.log(data);
 var content = ""
 for (let index = 0; index < data.projects.length; index++) {
     const element = data.projects[index];
     content += element.name + "\n"
 }
-document.body.innerText = content
+// document.body.innerText = content
 // fetch('../data.json')
 //     .then((response) => response.json())
 //     .then((json) => {
